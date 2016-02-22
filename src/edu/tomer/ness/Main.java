545cb4d1f2a269ee11b78f4f62fb9aafca7794be
nesscollege2016 <nesscollege2016@gmail.com>
2016-02-22 19:03:30 +0200

    test Primary numbers in a range of from -  to

--- src/edu/tomer/ness/Main.java
+++ src/edu/tomer/ness/Main.java
@@ -5,44 +5,41 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
 
-        /*
         System.out.println("Enter from:");
         int from = scan.nextInt();
 
         System.out.println("Enter to:");
         int to = scan.nextInt();
-*/
 
 
-        Scanner scan = new Scanner(System.in);
-
-
-        System.out.println("Enter a number to test:");
-        int currentNumber = scan.nextInt();
-
-        if (currentNumber < 2){
-            System.out.println(currentNumber + " not primary");
-        }else if(currentNumber == 2){
-            System.out.println(currentNumber + " Primary");
-        }else if(currentNumber %2 == 0){
-            System.out.println(currentNumber + " Not primary");
+        if (from <= 2){
+            from = 3;
+            System.out.print(2 + ", ");
+        }else if (from %2 == 0) {
+            from += 1;
         }
 
-        else{
-            boolean isPrimary = true;
-
-            for (int i = 3; i < currentNumber / 2; i+=2) {
-                if (currentNumber % i == 0){
-                    isPrimary = false;
-                    break;
+        //from izugi
+        //3, 5, 7, 9
+        for (int currentNumber = from; currentNumber < to; currentNumber+=2) {
+                boolean isPrimary = true;
+                for (int i = 3; i < currentNumber / 2; i += 2) {
+                    if (currentNumber % i == 0) {
+                        isPrimary = false;
+                        break;
+                    }
                 }
+                if (isPrimary)
+                    System.out.print(currentNumber + ", ");
             }
-
-            if (isPrimary)
-                System.out.println("Primary " + currentNumber);
-            else
-                System.out.println(currentNumber + " Not primary");
         }
+
+
+
+
+
+
     }
-}
+
