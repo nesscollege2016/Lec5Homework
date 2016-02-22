153eec797f203165ca82ce9b28dae7307ef49ebc
nesscollege2016 <nesscollege2016@gmail.com>
2016-02-22 19:15:00 +0200

    calculate powers using multiplication (x^y = ?)

--- src/edu/tomer/ness/Main.java
+++ src/edu/tomer/ness/Main.java
@@ -7,39 +7,21 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("Enter from:");
-        int from = scan.nextInt();
+        System.out.println("Enter x:");
+        int x = scan.nextInt();
+        int mult = 1;
 
-        System.out.println("Enter to:");
-        int to = scan.nextInt();
+        System.out.println("Enter y:");
+        int y = scan.nextInt();
 
-
-        if (from <= 2){
-            from = 3;
-            System.out.print(2 + ", ");
-        }else if (from %2 == 0) {
-            from += 1;
+        for (int i = 0; i < y ; i++) {
+            mult = mult * x;
         }
-
-        //from izugi
-        //3, 5, 7, 9
-        for (int currentNumber = from; currentNumber < to; currentNumber+=2) {
-                boolean isPrimary = true;
-                for (int i = 3; i < currentNumber / 2; i += 2) {
-                    if (currentNumber % i == 0) {
-                        isPrimary = false;
-                        break;
-                    }
-                }
-                if (isPrimary)
-                    System.out.print(currentNumber + ", ");
-            }
-        }
-
-
-
-
+        System.out.println(mult);
 
 
     }
+
+
+}
 
